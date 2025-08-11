1b1ac9dd458fbdfce78e3e9d94f2574e4eb1dd8b
praneeth116 <praneeth.n116@gmail.com>
2025-08-11 23:37:04 +0530

    add logoutUser api logic

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -53,7 +53,11 @@ const registerUser = asyncHandler(async(req, res) => {
 // route POST api/users/logout
 // @access public
 const logoutUser = asyncHandler(async(req, res) => {
-    res.status(200).json({message: "logout user"})
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    });
+    res.status(200).json({message: "Logged Out!"});
 })
 
 // @desc Get profile of the user
