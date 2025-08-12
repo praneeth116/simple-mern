e577aa6c14db7107ec19f6d80d50c5ae1f4ea699
praneeth116 <praneeth.n116@gmail.com>
2025-08-12 14:28:50 +0530

    fix auth middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,13 +1,14 @@
 import jwt from 'jsonwebtoken';
 import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
 
 const protect = asyncHandler(async(req, res, next) => {
-    let token;
-    token = res.cookie('jwt');
+    const token = req.cookies.jwt;
     if(token){
         try{
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.userId).select('-password');
+            next();
         }catch(err){
             res.status(401);
             throw new Error("Unauthorized, invalid token")
