09d469e69262bd0e90d6ebc28d4fb9ea2601f23b
praneeth116 <praneeth.n116@gmail.com>
2025-08-12 15:34:00 +0530

    add profile apis

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -64,14 +64,36 @@ const logoutUser = asyncHandler(async(req, res) => {
 // route GET api/users/profile
 // @access private
 const getUserProfile = asyncHandler(async(req, res) => {
-    res.status(200).json({message: "user profile"})
+    const user = {
+        _id: req.user._id,
+        name: req.user.name,
+        email: req.user.email
+    };
+    res.status(200).json(user);
 })
 
 // @desc Get profile of the user
 // route PUT api/users/profile
 // @access private
 const updateUserProfile = asyncHandler(async(req, res) => {
-    res.status(200).json({message: "update user profile"})
+    const user = await User.findById(req.user._id);
+    if(user){
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        if(req.body.password){
+            user.password = req.body.password;
+        }
+        const updateUser = await user.save();
+        res.status(200).json({
+            _id  : updateUser._id,
+            name : updateUser.name,
+            email: updateUser.email 
+        });
+    }
+    else{
+        res.status(404);
+        throw new Error("User not found!");
+    }
 })
 
 export {
